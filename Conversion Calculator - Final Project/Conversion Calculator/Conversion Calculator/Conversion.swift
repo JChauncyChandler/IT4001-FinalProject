7fe3a61fcd9d18b0459ebff1f5502b4acb6fe226
Jackson Chandler <jackson.c.chandler@gmail.com>
2017-12-11 20:34:17 -0600

    init

---
+++ Conversion Calculator - Final Project/Conversion Calculator/Conversion Calculator/Conversion.swift
@@ -0,0 +1,20 @@
+//
+//  Conversion.swift
+//  Conversion Calculator
+//
+//  Created by Chandler, Jackson C. (MU-Student) on 12/7/17.
+//  Copyright © 2017 Chandler, Jackson C. (MU-Student). All rights reserved.
+//
+
+import Foundation
+
+struct Conversion{
+    let original: String
+    let converted: String
+    init(original: String, converted: String) {
+        self.original = original
+        self.converted = converted
+    }
+}
+
+
---
+++ Conversion Calculator - Final Project/Conversion Calculator/Conversion Calculator/TitleChanger.swift
@@ -0,0 +1,18 @@
+//
+//  TitleChanger.swift
+//  Conversion Calculator
+//
+//  Created by Chandler, Jackson C. (MU-Student) on 12/7/17.
+//  Copyright © 2017 Chandler, Jackson C. (MU-Student). All rights reserved.
+//
+
+import Foundation
+
+struct TitleChanger{
+    let inputTitle: String
+    let outputTitle: String
+    init(inputTitle: String, outputTitle: String) {
+        self.inputTitle = inputTitle
+        self.outputTitle = outputTitle
+    }
+}
